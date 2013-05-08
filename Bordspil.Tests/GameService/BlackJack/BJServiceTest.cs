0ade6cbaccea0ca9401c85e4f4aae752aba79a39
JonkiH <jonkihe@gmail.com>
2013-05-08 16:24:25 +0000

    UnitTest for BlackJack: Testing for CalculateSum

--- Bordspil.Tests/GameService/BlackJack/BJServiceTest.cs
+++ Bordspil.Tests/GameService/BlackJack/BJServiceTest.cs
@@ -11,15 +11,28 @@ namespace Bordspil.Tests.GameService.BlackJack
     [TestClass]
     public class BJServiceTest
     {
-        BJService test = new BJService();
+        BJService bjTest = new BJService();
+
+        List<int> testList1 = new List<int> { 1, 14, 27, 40 }; // 4 Ace
+        List<int> testList2 = new List<int> { 1, 10, 26 };  // ace, 10, K
+        List<int> testList3 = new List<int> { 10, 51, 43 };  // 10 , Q and 4
+
         [TestMethod]
         public void TestAce()
         {
-            Assert.AreEqual(test.IsAce(1), true); // is an Ace
-            Assert.AreEqual(test.IsAce(14), true); // is an Ace
-            Assert.AreEqual(test.IsAce(42), false); // is not an Ace
+            Assert.AreEqual(bjTest.IsAce(1), true); // is an Ace
+            Assert.AreEqual(bjTest.IsAce(14), true); // is an Ace
+            Assert.AreEqual(bjTest.IsAce(42), false); // is not an Ace
         }
-    
+
+        [TestMethod]
+        public void TestCalculateSum()
+        {
+            Assert.AreEqual(bjTest.CalculateSum(testList1), 14);
+            Assert.AreEqual(bjTest.CalculateSum(testList2), 31); 
+            Assert.AreEqual(bjTest.CalculateSum(testList3), 24); 
+        }
+
 
     }
 }
