e8e1dc7ac1de853e243fb8404d1720f9fb7c7973
JonkiH <jonkihe@gmail.com>
2013-05-08 16:08:53 +0000

    Making UnitTest  class for BJService

--- Bordspil.Tests/GameService/BlackJack/BJServiceTest.cs
+++ Bordspil.Tests/GameService/BlackJack/BJServiceTest.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bordspil;
+using Bordspil.GameService;
 
 namespace Bordspil.Tests.GameService.BlackJack
 {
     [TestClass]
     public class BJServiceTest
     {
+        BJService test = new BJService();
         [TestMethod]
-        public void TestMethod1()
+        public void TestAce()
         {
+            Assert.AreEqual(test.IsAce(0), true); // is an Ace
+            Assert.AreEqual(test.IsAce(13), true); // is an Ace
+            Assert.AreEqual(test.IsAce(42), false); // is not an Ace
         }
+    
     }
 }
