41cd292963e7a33102ee22e5a3d83f4357cfb52c
JonkiH <jonkihe@gmail.com>
2013-05-08 16:05:54 +0000

    UnitTest for BJService.IsAce

---
+++ Bordspil.Tests/GameService/BlackJack/BJServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bordspil.Tests.GameService.BlackJack
+{
+    [TestClass]
+    public class BJServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
