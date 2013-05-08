0079d388de58155ae979e2a3d7fd8de1b6ed7ca8
arnorb <arnor@arnor.is>
2013-05-08 09:47:10 +0000

    commenting

--- Bordspil/Controllers/HomeController.cs
+++ Bordspil/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace Bordspil.Controllers
     {
         public ActionResult Index()
         {
+            // hvað er í gangi?
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
