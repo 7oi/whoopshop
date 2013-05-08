f772b075cef944e098ed04a962e2c2c6b5c372f0
arnorb <arnor@arnor.is>
2013-05-08 12:30:16 +0000

    Added class RiskRepository

---
+++ Bordspil/Models/RiskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity
+
+namespace Bordspil.Models
+{
+    public class RiskRepository : GameRepository
+    {
+        public int landID { get; set; }
+        public string landName { get; set; }
+        public int continentID { get; set; }
+        public List<landID> neighbouringLands { get; set; }
+        public int occupier { get; set; }
+        public int numberOfTroops { get; set; }
+
+    }
+}
