af4abb1237a4b57063eeba9ddf258eda1a14358e
7oi <sevenoi@sevenoi.com>
2013-05-08 09:05:31 +0000

    Grunnurinn kominn

---
+++ Bordspil/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bordspil.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewGame(int type)
+        {
+            return View();
+        }
+
+        public ActionResult Play(int gameID)
+        {
+            return View();
+        }
+
+        public ActionResult View(int gameID)
+        {
+            return View();
+        }
+    }
+}
