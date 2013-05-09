cea525d17262fa82dd87e1ea73016be35906bb09
JonkiH <jonkihe@gmail.com>
2013-05-09 10:30:10 +0000

    Added function
    MultipleDiceTrow() return List of int
    Change function
    AllocateTroops() it now return one int
    DiceTrow() return rancom int
    
    Put summery on them

--- Bordspil/GameService/Risk/RiskService.cs
+++ Bordspil/GameService/Risk/RiskService.cs
@@ -11,7 +11,6 @@ namespace Bordspil.GameService
         #region StartGameVariables
         public Random startPlayer = new Random();
         public int numTroops;
-        public Random dice = new Random();
         #endregion
 
         #region OneTurnVariables
@@ -29,43 +28,11 @@ namespace Bordspil.GameService
         #region HelperFunctions
 
 
-        public int ThrowDice()
-        {
-            return dice.Next(1, 6);
-        }
-
         public int WhoStarts(int numPlayer)
         {
             return startPlayer.Next(1, numPlayer);
         }
 
-        public void AllocateTroops(int numPlayer)
-        {
-            switch (numPlayer)
-            {
-                case 2:
-                    {
-                        numTroops = 40;
-                        return;
-
-                    }
-                case 3:
-                    {
-                        numTroops = 35;
-                        return;
-                    }
-                case 4:
-                    {
-                        numTroops = 30;
-                        return;
-                    }
-                case 5:
-                    {
-                        numTroops = 25;
-                        return;
-                    }
-            }
-        }
 
         public void PlaceFirstTroops(int numTroops)
         {
@@ -73,19 +40,19 @@ namespace Bordspil.GameService
             return;
         }
 
-        
+
         public void AddTroops()
         {
-            CalculateReinforcements ();
-            PlaceTroops (reinforcement);
+            CalculateReinforcements();
+            PlaceTroops(reinforcement);
         }
-        
+
         public int CalculateReinforcements()
         {
             ContinentConquerer();
             return 0;
         }
-    
+
         public int ContinentConquerer()
         {
             return 0;
@@ -176,8 +143,73 @@ namespace Bordspil.GameService
             }
 
         }
+
+
+
+        #endregion
+
+        #region First Round Function
+
+        /// <summary>
+        /// Resturn number of troops after how many players are playing
+        /// </summary>
+        /// <param name="numPlayer"></param>
+        /// <returns></returns>
+        public int AllocateTroops(int numPlayer)
+        {
+            switch (numPlayer)
+            {
+                case 2:
+                    {
+                        return 40;
+                    }
+                case 3:
+                    {
+                        return 35;
+                    }
+                case 4:
+                    {
+                        return 30;
+                    }
+                case 5:
+                    {
+                        return 25;
+                    }
+            }
+            return 0;
         }
 
         #endregion
 
+        #region RandomFunction
+
+        /// <summary>
+        /// Behave like 6 side dice
+        /// </summary>
+        /// <returns></returns>
+        public int ThrowDice()
+        {
+            Random dice = new Random();
+            return dice.Next(1, 6);
+        }
+
+        /// <summary>
+        /// Take in number of dice and return List of dice role
+        /// </summary>
+        /// <param name="numTroops"></param>
+        /// <returns></returns>
+        public List<int> MultipleThrowDice(int numDice)
+        {
+            List<int> dices = new List<int>();
+            for (int i = 0; i < numDice; i++)
+            {
+                dices.Add(ThrowDice());
+            }
+            return dices;
+        }
+
+
+        #endregion
     }
+
+}
